583872b4020adc6787410faa93d67ddc112a4dfd
rlmgtr <rlmagtira@gmail.com>
2024-10-13 02:12:21 +0800

    Trial

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,11 @@
-import Header from "./Components/Header";
+import Header from "./Header";
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import About from "./Pages/About";
-import Home from "./Pages/Home";
-import Contact from "./Pages/Contact";
-import Portfolio from "./Pages/Portfolio";
-import Footer from "./Components/Footer";
+import About from "../Pages/About";
+import Home from "../Pages/Home";
+import Contact from "../Pages/Contact";
+import Portfolio from "../Pages/Portfolio";
+import Footer from "./Footer";
 
 function App() {
   return (
