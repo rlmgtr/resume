2eefa315ef90a1ee03faa973530b382c318a6a6f
rlmgtr <rlmagtira@gmail.com>
2024-10-12 16:03:40 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./Components/Header";
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import About from "./Pages/About";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
