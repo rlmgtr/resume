abeaf394c60093ba2ceb21a2f648c7f567b6a725
rlmgtr <rlmagtira@gmail.com>
2024-10-28 00:18:33 +0800

    update - Home
    planning to combine home and contact

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,45 @@
 const Home = () => {
   return (
     <div>
-      Home Page - this text is for test! 
-      additional for change test
+
+
+
+
+      <div>Home Page - this text is for test! 
+      additional for change test </div>
+
+
+
+<div>
+  <img src="me" alt="me"/>
+</div>
+
+
+<div>
+
+
+Technologies
+
+<ul>
+  <li>MongoD</li>
+  <li>Express JS</li>
+  <li>React JS</li>
+  <li>Node JS</li>
+</ul>
+
+
+
+
+</div>
+
+
     </div>
+
+
+
+
+
+
   )
 }
 
