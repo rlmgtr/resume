d30a65e9b60df681638028d6a9edb3255a493df7
rlmgtr <rlmagtira@gmail.com>
2024-11-12 12:58:33 +0800

    update contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,11 +1,48 @@
-
+import css from './CSS/Contact.css'
 
 const Contact = () => {
   return (
-    <div>
+<div>
+
+
+   <div className="contactIntro">
       Hello, I am seeking for employment as a Junior or assistant Junior Web Developer. I hope you will provide me an oppurtunity to contribute and grow at the same time in programming industry. 
 
       Should you have further question you can send me a message
+    </div>
+
+<div className='contactinfo'>
+Email
+</div>
+
+<div className='contactinfo'>
+number
+</div>
+
+<div className='contactinfo'>
+LinkedIn
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
