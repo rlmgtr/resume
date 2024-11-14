d743e9aa88705d7ce5b3a0e022094fd54c2eb35f
rlmgtr <rlmagtira@gmail.com>
2024-11-14 16:14:58 +0800

    homepage
    Design updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,14 +9,13 @@ import html from '../images/About/html.jpg';
 import me from '../images/Home/me.png';
 import me2 from '../images/Home/me2.png';
 
-const homeBg = {
-  
-};
+
+
 
 
 const Home = () => {
   return (
-    <div className='homeWrapper' style={homeBg}>
+    <div className='homeWrapper'>
 
 <div className='meImg'> <img src={me2} height={800} width={600} /></div>
 
@@ -25,21 +24,20 @@ const Home = () => {
 <div className="techs">
 
 
+<div className='intro'>Hey there! My name is Luis, and I'm an aspiring Junior Web Developer. Feel free to browse my portfolio. I made it a bit interactive, so enjoy hovering or clicking most elements if you're on mobile to learn more. Thank you!</div>
+
 <ul className='tech-list'>
 
 
 <li>
 <div className="tech-item">
 <img src={mongoDB} width={75} height={75} alt='mongoDB' className='techImg'/> 
+
 </div>
 </li>
 
-
-
-
 <li>
-<div className="tech-item">
-    
+<div className="tech-item">    
 <img src={expressJS} width={75} height={75} alt='expressJS' className='techImg'/> 
 </div>
 </li>
@@ -75,24 +73,38 @@ const Home = () => {
 <img src={html} width={75} height={75} alt='html' className='techImg'/> 
 </div>
 </li>
-
-
 </ul>
 
+<div>
+  When learning how to code,  aside from programming languages I also learned about the following: 
+</div>
 
+<div className='quotes'>
+  
+  <li className='quote'>
+    <div className='qImg'>image here</div>
+    <div className='qWords'>Continious practice and research are essential elements to be an efficient programmer</div>
+    
+  </li>
 
+  <li className='quote'>
+  <div className='qImg'>image here</div>
+  <div className='qWords'>One of the important mindset to learn more is to always acknowledge that you know less</div>
+     </li>
+
+  <li className='quote'>
+  <div className='qImg'>image here</div>
+  <div className='qWords'>I realize that coding is never easy - But heck! It is really fulfilling</div>
+  </li>
 
 </div>
 
 
+  </div>
 
 
 
-
-
-
-
-    </div>
+  </div>
   )
 }
 
