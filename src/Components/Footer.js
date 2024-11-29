6c38d7fdc946c429811ab80b697c5cc9f4edfa18
rlmgtr <rlmagtira@gmail.com>
2024-11-29 12:19:04 +0800

    Updates

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,9 +6,12 @@ const Footer = () => {
       
 
         <ul className='socials'>
-            <li>LinkedIn</li>
-            <li>Facebook</li>
-            <li>Github</li>
+        <li>-Thank You for supporting me to be part programming community-</li>
+        <li>To God, thank you for leading me to this opportunity.</li>
+        <li>My ever supportive wife, Ann.</li>
+        <li>To my mentors and classmates from GoIT Phillippines.</li>
+        <li>To all the available public sources and community of WebDev</li>
+        <li>Mr.Abdul Gofur for the amazing webiste background.</li>
         </ul> 
 
 
